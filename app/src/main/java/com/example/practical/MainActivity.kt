32fbc6a3ac2bed4625a73be58876ee33c7a1b9de
YIPZIYAN <103913961+YIPZIYAN@users.noreply.github.com>
2023-05-02 19:41:40 +0800

    a

--- app/src/main/java/com/example/practical/MainActivity.kt
+++ app/src/main/java/com/example/practical/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.practical
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
+        val navControllerManager = supportFragmentManager.findFragmentById(R.id.fragmentContainerView2) as NavHostFragment
+    val navController = navControllerManager.navController
+val appBarConfiguration = AppBarConfiguration(navController.graph)
+setupActionBarWithNavController(navController,appBarConfiguration)
+
     }
 }
